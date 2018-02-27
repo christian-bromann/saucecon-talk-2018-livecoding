4adb0bc0dcbe7f197d4823bfe572ea2b260a6d35
christian-bromann <mail@christian-bromann.com>
2018-02-26 17:39:15 -0800

    add example tests

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -24,5 +24,13 @@ exports.config = Object.assign(wdioConf.config, {
     //
     capabilities: [{
         browserName: 'chrome'
-    }]
+    }],
+    //
+    // Test runner services
+    // Services take over a specific job you don't want to take care of. They enhance
+    // your test setup with almost no effort. Unlike plugins, they don't add new
+    // commands. Instead, they hook themselves up into the test process.
+    services: ['chromedriver'],
+    port: 9515,
+    path: '/'
 })
